50f862766edc15bf18eed5720db7eaadbe93a144
Dmitri Mesin <dmitriym@elance-odesk.com>
2015-01-25 02:16:11 +0200

    new version

--- index.js
+++ index.js
@@ -8,21 +8,36 @@ module.exports.extension = function() {
     global.i18n = require('i18n');
 
     var app = twee.getApplication()
-        , initOptions = twee.getConfig('twee:options:i18n:init');
+        , initOptions = twee.getConfig('extension:twee-i18n:init');
 
-    initOptions["updateFiles"] = (app.get('env') == 'development' && twee.getConfig('twee:options:i18n:autoUpdate'));
+    initOptions["updateFiles"] = (app.get('env') == 'development' && twee.getConfig('extension:twee-i18n:autoUpdate'));
     i18n.configure(initOptions);
 
     app.use(i18n.init);
 
     app.use(function (req, res, next) {
         app.locals.tr = global.tr = res.__;
+        app.locals.i18n = global.i18n = res.i18n;
         next();
     });
 };
 
 module.dependencies = {
-    "Twee HTTP Parser": {
+    "HTTP Parser": {
         "module": "twee-http-parser-extension"
     }
+};
+
+
+module.exports.configNamespace = 'twee-i18n';
+
+module.exports.config = {
+    "init": {
+        locales:['en', 'ru']
+        , defaultLocale: 'en'
+        , cookie: 'locale'
+        , directory: 'i18n'
+        , updateFiles: false
+    },
+    "autoUpdate": false
 };
